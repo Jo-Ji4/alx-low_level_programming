f14ed393ed859d81eaf1bfe7dab714c5a66d49e0
Jo-Ji4 <joten1000@gmail.com>
2023-03-20 16:51:55 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@ int main(void)
 	int y;
 	long long int z;
 	float F;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(x));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(F));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
