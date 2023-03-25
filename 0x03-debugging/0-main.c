23c7eb266b8e3821963b521e888e7efc9f220735
Jo-Ji4 <joten1000@gmail.com>
2023-03-24 18:48:04 -0700

    all tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - codes that prints if num is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Inft loop
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("Inft loop avoided! \\o/\n");
+
+	return (0);
+}
